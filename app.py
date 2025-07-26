ae2961c98f92c6429d6b577c7e419c5c205780e7
sunkanmyt <adesunkanmy@gmail.com>
2025-07-26 11:00:28 +0100

    Login and admissions routes and pages

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "USA" # Must change later
+
 
 # ROUTES
 @app.route("/")
@@ -26,7 +28,7 @@ def contact():
         message = request.form["message"]
 
         # Add data to database  
-        conn = sqlite3.connect("contact.db")
+        conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
         cursor.execute("INSERT INTO messages (name, email, message) VALUES (?, ?, ?)", (name, email, message))
         conn.commit()
@@ -41,7 +43,10 @@ def thank_you():
     
 @app.route("/admin/messages")
 def view_messages():
-    conn = sqlite3.connect("contact.db")
+    if not session.get("admin_logged_in"):
+        return redirect(url_for("login"))
+    
+    conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     cursor.execute("SELECT id, name, email, message FROM messages")
     messages = cursor.fetchall()
@@ -49,9 +54,36 @@ def view_messages():
 
     return render_template("messages.html", messages=messages)
 
+@app.route("/admin/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM admin_users WHERE username=? AND password=?", (username, password))
+        user = cursor.fetchone()
+        conn.close()
+        
+        if user:
+            session["admin_logged_in"] = True
+            return (redirect(url_for("view_messages")))
+        else:
+            flash('Invalid credentials')
+            # return redirect(url_for("login"))
+        
+    return render_template("login.html")
+
+@app.route("/admin/logout")
+def logout():
+    session.pop("admin_logged_in", None)
+    return redirect(url_for("login"))
+
+
 # DATABASE SETUP FUNCTION
 def init_db():
-    conn = sqlite3.connect("contact.db")
+    conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS messages (
@@ -60,8 +92,15 @@ def init_db():
         email TEXT NOT NULL, 
         message TEXT NOT NULL)
     """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS admin_users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        username TEXT UNIQUE NOT NULL, 
+        password TEXT NOT NULL)
+    """)
     conn.commit()
     conn.close()
+
 
 # MUST RUN BEFORE THE START OF APP TO INITIALIZE THE DATABASE
 init_db()
